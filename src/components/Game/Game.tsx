9725a38a366acdbf3fc97c684e11b7a88f60e0f8
kevinchang <kevinjchang98@gmail.com>
2022-09-02 17:30:26 -0700

    begin implementing basic functions

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,17 +1,38 @@
 import { Canvas } from '@react-three/fiber';
+import { useState } from 'react';
 
 import styles from './Game.module.css';
 
 function Game() {
+    // Keep track of current roll
+    const [roll, setRoll] = useState<number>(0);
+    const [currPlayer, setCurrPlayer] = useState<number>(0);
+
+    const rollDice = () => {
+        // TODO: Actually generate a new roll
+        setRoll(roll + 1);
+
+        // Swap current player
+        setCurrPlayer(currPlayer == 0 ? 1 : 0);
+    };
+
     return (
-        <div className={styles.canvasContainer}>
-            <Canvas>
-                <mesh>
-                    <boxGeometry />
-                    <meshStandardMaterial />
-                </mesh>
-            </Canvas>
-        </div>
+        <>
+            <div className={styles.canvasContainer}>
+                <Canvas>
+                    <mesh>
+                        <boxGeometry />
+                        <meshStandardMaterial />
+                    </mesh>
+                </Canvas>
+            </div>
+
+            <div>
+                <p>Current roll: {roll}</p>
+                <p>Current player: {currPlayer}</p>
+                <button onClick={rollDice}>Roll</button>
+            </div>
+        </>
     );
 }
 
